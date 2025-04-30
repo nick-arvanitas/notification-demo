7e18f37a21273cae6367baa54e35639b60d67b18
nick-arvanitas <nick.arvanitas@highwire.com>
2025-04-30 16:22:50 -0600

    another options

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -55,7 +55,7 @@ function AccordionContent({
   return (
     <AccordionPrimitive.Content
       data-slot="accordion-content"
-      className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm px-5"
+      className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-[16px] px-5"
       {...props}
     >
       <div className={cn('pt-0 pb-4', className)}>{children}</div>
